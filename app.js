ef9bcd4553fa64ae1ff385747f741cdd33000613
Kevin <kevinmvetter@gmail.com>
2022-09-22 21:22:35 +0200

    made some more shuffling

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
-console.log("Hello World!");
-
-let suits = [1,2,3,4,5,6,7,8,9,10,11,12,13];
-
-
-let rnd = Math.floor(Math.random() * 10) + 1;
-for (let i=0; i < Math.floor(Math.random() * 10) + 1; i++){
-    suits = Shuffle(suits);
+function ShuffleRandom(arr){
+    let rnd = Math.floor(Math.random() * 10) + 1;
+    for (let i=0; i < rnd; i++){
+        arr = Shuffle(arr);
+    }
+    return arr;
 }
-
 function Shuffle(arr){
     let shuffledArr = [];
     let length = arr.length;
